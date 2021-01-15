ae145e9c40407abaf7c0e59d275491b0acbd113e
tyfts1 <romannawaz01@gmail.com>
2021-01-15 13:12:14 +0200

    Initial commit

---
+++ main.js
@@ -0,0 +1,53 @@
+function CoffeeMake() { }
+CoffeeMake.prototype = {
+    on: function () {
+        return `${this.model} is on`;
+    },
+    off: function () {
+        return `${this.model} is off`;
+    }
+}
+
+let coffeeMake = new CoffeeMake();
+
+function DripCoffeeMaker(model) {
+    this.model = model;
+    this.createCoffee = function () {
+        return 'Create Drip Coffee';
+    }
+}
+DripCoffeeMaker.prototype = coffeeMake;
+
+let dripCoffeeMaker = new DripCoffeeMaker('Drip Coffee');
+
+console.log(dripCoffeeMaker.on());
+console.log(dripCoffeeMaker.off());
+console.log(dripCoffeeMaker.createCoffee());
+
+function CorabCoffeeMaker(model) {
+    this.model = model;
+    this.createCoffee = function () {
+        return 'Create Corab Coffee';
+    }
+}
+CorabCoffeeMaker.prototype = coffeeMake;
+
+let corabCoffeeMaker = new CorabCoffeeMaker('Corab Coffee');
+
+console.log(corabCoffeeMaker.on());
+console.log(corabCoffeeMaker.off());
+console.log(corabCoffeeMaker.createCoffee());
+
+function CoffeeMachine(model) {
+    this.model = model;
+    this.createCoffee = function () {
+        return 'Coffee Machine';
+    }
+}
+CoffeeMachine.prototype = coffeeMake;
+
+let coffeeMachine = new CoffeeMachine('Coffee Machine');
+
+console.log(coffeeMachine.on());
+console.log(coffeeMachine.off());
+console.log(coffeeMachine.createCoffee());
